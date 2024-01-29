2f33660f2866a165609a45ef87181ef9413d4240
NikolasSko <Skobelev.nikita12345@mail.ru>
2024-01-29 13:37:58 +0700

    Условный рендеринг

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ let app = new Vue({
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         linkText: 'More products like this',
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        onSaleText: 'On Sale',
+
+
 
     }
 })
